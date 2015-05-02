b531778f72d2ef42ffa40d9f302c570821f5c20a
Milos Stojanovic <milos92s@yahoo.com>
2015-05-02 23:54:06 +0200

    Added insert builder.

--- src/com/github/somi92/sqldbb/query/builder/InsertQueryBuilder.java
+++ src/com/github/somi92/sqldbb/query/builder/InsertQueryBuilder.java
@@ -5,10 +5,67 @@
  */
 package com.github.somi92.sqldbb.query.builder;
 
+import com.github.somi92.sqldbb.entity.DatabaseEntity;
+import com.github.somi92.sqldbb.query.Query;
+import java.util.List;
+
 /**
  *
  * @author milos
  */
-public class InsertQueryBuilder {
+public class InsertQueryBuilder implements IQueryBuilder {
+    
+    private Query query;
+    
+    public InsertQueryBuilder() {
+        query = new Query();
+    }
+
+    @Override
+    public void setKeywords() {
+        String[] keywords = new String[3];
+        keywords[0] = "INSERT";
+        keywords[1] = "INTO";
+        keywords[2] = "VALUES";
+        query.setKeywords(keywords);
+    }
+
+    @Override
+    public void setQueryFormat() {
+        String format = "%s %s %s %s %s %s;";
+        query.setFormat(format);
+    }
+
+    @Override
+    public void formatQuery(DatabaseEntity dbe) {
+        String tableName = dbe.getTableName();
+        
+        List<String> columns = dbe.getAllColumns();
+        String columnsForQuery = "(";
+        String valuesForQuery = "(";
+        for(int i=0; i<columns.size(); i++) {
+            if(i==(columns.size()-1)) {
+                columnsForQuery += columns.get(i)+")";
+                valuesForQuery += "?)";
+            } else {
+                columnsForQuery += columns.get(i)+", ";
+                valuesForQuery += "?, ";
+            }
+        }
+        
+        String queryValue = String.format(query.getFormat(),
+                query.getKeywords()[0],
+                query.getKeywords()[1],
+                tableName,
+                columnsForQuery,
+                query.getKeywords()[2],
+                valuesForQuery);
+        query.setQuery(queryValue);
+    }
+
+    @Override
+    public Query getQuery() {
+        return query;
+    }
     
 }
